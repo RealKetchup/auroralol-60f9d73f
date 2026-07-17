98ef9cb51ff796652dd73184dfd4f6c43746299a
KetchupBottle <epiknezure@gmail.com>
2026-07-18 00:18:12 +0300

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -95,7 +95,7 @@ function RootShell({ children }: { children: ReactNode }) {
     </html>
   );
 }
-const MAINTENANCE_MODE = false;
+const MAINTENANCE_MODE = true;
 function RootComponent() {
   if (MAINTENANCE_MODE) {
     return <Maintenance />;
